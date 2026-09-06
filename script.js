00e21fdd918d205dbe4245784bf09c2323a27313
CarlettiFrancesco <francesco.carletti2008@gmail.com>
2026-09-06 16:52:29 +0200

    colori e tasto inizio

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ let numCaterpillars = 4;
 let caterpillarEnds = [];
 
 let isRacing = false;
+let startButton;
 
+let colors = [
+  [255, 215, 0],
+  [255, 0, 0],
+  [0, 255, 0],
+  [0, 0, 255],
+];
 
 function setup() {
   createCanvas(500, 500);
- 
   frameRate(3);
 
-  for (let i=0;i<numCaterpillars;i++) {
-    caterpillarEnds.push(startLine)
+  for (let i = 0; i < numCaterpillars; i++) {
+    caterpillarEnds.push(startLine);
   }
+
+  startButton = createButton("START");
+  startButton.center();
+  startButton.position(200, 600);
+  
+  startButton.mousePressed(startRace);
 }
 
 function draw() {
@@ -32,13 +44,16 @@ function draw() {
   if (isRacing === true) {
     moveCaterpillars();
   } else { 
-
     writeStart();
   }
 
   drawCaterpillars();
- 
   checkWinner();
+}
+
+function startRace() {
+  isRacing = true;
+  startButton.hide();
 }
 
 function writeStart() {
@@ -47,12 +62,12 @@ function writeStart() {
   fill(255);
   noStroke();
 
-  text("Click to start!", width / 2, height / 2);
+  text("Premi il bottone START per iniziare!", width / 2, height / 2 - 20);
 }
 
-function drawCaterpillar(x, y, segments) {
+function drawCaterpillar(color, x, y, segments) {
   for (let i = 0; i < segments; i += 1) {
-    fill(255, 0, 200);
+    fill(color[0], color[1], color[2]);
     stroke(0);
     strokeWeight(1);
     circle(x, y, 50);
@@ -70,21 +85,15 @@ function drawCaterpillars() {
   let padding = height / numCaterpillars;
   for (let i = 0; i < numCaterpillars; i += 1) {
     let y = (i + 0.5) * padding;
-  
-    crawl = random(3, 6);
+    let crawl = random(3, 6);
 
-    drawCaterpillar(caterpillarEnds[i], y, crawl);
+    drawCaterpillar(colors[i], caterpillarEnds[i], y, crawl);
   }
-}
-
-function mousePressed() {
-  isRacing = true;
 }
 
 function moveCaterpillars() {
   for (let i = 0; i < numCaterpillars; i += 1) {
     let move = round(random(5, 30));
- 
     caterpillarEnds[i] += move;
   }
 }
@@ -97,8 +106,7 @@ function checkWinner() {
       fill(255);
       noStroke();
 
-      text(`Caterpillar ${i + 1} wins!`,width/2,height/2);
-
+      text(`Caterpillar ${i + 1} wins!`, width / 2, height / 2);
       noLoop();
     }
   }
